e71d4ead9775f83e8a6a8c7440febbbeaee97406
vitkarpov <viktor.s.karpov@gmail.com>
2022-12-01 09:02:47 +0000

    Day 1, part 2

--- 1.js
+++ 1.js
@@ -16,4 +16,10 @@ function part1() {
   return Math.max(...data.map((pack) => pack.reduce((acc, v) => acc + v, 0)));
 }
 
-console.log(part1());
+function part2() {
+  const data = read('day1-1.txt');
+  const [t1, t2, t3] = data.map((pack) => pack.reduce((acc, v) => acc + v, 0)).sort((a, b) => b - a);
+  return t1 + t2 + t3;
+}
+
+console.log(part2());
