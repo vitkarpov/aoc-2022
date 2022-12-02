b02e910cacec1c14459b52581c78b77a2b9fadca
vitkarpov <viktor.s.karpov@gmail.com>
2022-12-02 11:00:45 +0000

    Day 2, part 2

--- 2.js
+++ 2.js
@@ -1,26 +1,26 @@
 import fs from 'fs';
 
-// A, X - rock
-// B, Y - paper
-// C, Z - scissors
+// A - rock, X - lose
+// B - paper, Y - draw
+// C - scissors, Z - win
 
 const scores = {
-  'A X': 1 + 3,
-  'A Y': 2 + 6,
-  'A Z': 3 + 0,
+  'A X': 3 + 0,
+  'A Y': 1 + 3,
+  'A Z': 2 + 6,
   'B X': 1 + 0,
   'B Y': 2 + 3,
   'B Z': 3 + 6,
-  'C X': 1 + 6,
-  'C Y': 2 + 0,
-  'C Z': 3 + 3,
+  'C X': 2 + 0,
+  'C Y': 3 + 3,
+  'C Z': 1 + 6,
 };
 
-function part1() {
+function part2() {
   return fs.readFileSync('day2-1.txt', 'utf-8').split('\n').reduce((acc, k) => {
     acc += scores[k];
     return acc;
   }, 0);
 }
 
-console.log(part1());
+console.log(part2());
