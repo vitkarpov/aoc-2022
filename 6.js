501bce3d1375a3d73db9e3686a641826fafb6901
vitkarpov <viktor.s.karpov@gmail.com>
2022-12-06 09:32:00 +0000

    Day 6, part 2

--- 6.js
+++ 6.js
@@ -1,26 +1,26 @@
 import assert from 'assert';
 import fs from 'fs'
 
-function part1() {
+function part2() {
   const s = read();
   const w = {};
   for (let i = 0; i < 26; i++) {
     w[String.fromCharCode('a'.charCodeAt(0) + i)] = 0;
   }
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < 14; i++) {
     w[s[i]]++;
   }
-  for (let i = 4; i < s.length; i++) {
-    w[s[i - 4]]--;
+  for (let i = 14; i < s.length; i++) {
+    w[s[i - 14]]--;
     w[s[i]]++;
     if (Object.values(w).every((v) => v === 1 || v === 0)) {
       return i + 1;
     }
   }
-  assert(false, 'No start marker');
+  assert(false, 'No message pack marker');
 }
 
-console.log(part1())
+console.log(part2())
 
 function read() {
   return fs.readFileSync('day6.txt', 'utf-8');
