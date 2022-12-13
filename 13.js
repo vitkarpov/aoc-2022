eb31034714830f48e319a9f39aa17640518a0e0b
vitkarpov <viktor.s.karpov@gmail.com>
2022-12-13 09:05:30 +0000

    Refactor for part 2

--- 13.js
+++ 13.js
@@ -2,44 +2,43 @@ import fs from 'fs';
 
 function read() {
   return fs.readFileSync('day13.txt', 'utf-8').split('\n').reduce((acc, line) => {
-    if (!line) {
-      acc.push({ l: null, r: null });
-    } else if (!acc[acc.length - 1].l) {
-      acc[acc.length - 1].l = eval(line);
-    } else {
-      acc[acc.length - 1].r = eval(line);
+    if (line) {
+      acc.push(eval(line));
     }
     return acc;
-  }, [[]])
+  }, [])
 }
 
 function part1() {
-  function compare(l, r) {
-    if (Array.isArray(l) && !Array.isArray(r)) {
-      return compare(l, [r]);
+  const packets = read();
+  let acc = 0;
+  for (let i = 0; i < packets.length - 1; i += 2) {
+    if (compare(packets[i], packets[i + 1]) > 0) {
+      acc += i / 2 + 1;
     }
-    if (Array.isArray(r) && !Array.isArray(l)) {
-      return compare([l], r);
+  }
+  return acc;
+}
+
+function compare(l, r) {
+  if (Array.isArray(l) && !Array.isArray(r)) {
+    return compare(l, [r]);
+  }
+  if (Array.isArray(r) && !Array.isArray(l)) {
+    return compare([l], r);
+  }
+  if (Array.isArray(l)) {
+    for (let i = 0; i < Math.min(l.length, r.length); i++) {
+      const v = compare(l[i], r[i]);
+      if (v !== 0) return v;
     }
-    if (Array.isArray(l)) {
-      for (let i = 0; i < Math.min(l.length, r.length); i++) {
-        const v = compare(l[i], r[i]);
-        if (v !== 0) return v;
-      }
-      if (l.length < r.length) return 1;
-      if (l.length > r.length) return -1;
-      return 0;
-    }
-    if (l < r) return 1;
-    if (l > r) return -1;
+    if (l.length < r.length) return 1;
+    if (l.length > r.length) return -1;
     return 0;
   }
-  return read().reduce((acc, packets, idx) => {
-    if (compare(packets.l, packets.r) > 0) {
-      acc += idx + 1;
-    }
-    return acc;
-  }, 0);
+  if (l < r) return 1;
+  if (l > r) return -1;
+  return 0;
 }
 
 console.log(part1());
